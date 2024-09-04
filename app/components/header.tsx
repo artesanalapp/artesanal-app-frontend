d16ac45f5c33ca603d1bebd8b61a07ba5705290c
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2024-09-03 19:52:30 -0500

    refactor layout, styles, menu

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,33 +1,15 @@
 "use client";
-import React, { useState } from "react";
-import MenuModal from "@/app/components/menuModal";
+import React from "react";
+
+import SideNav from "@/app/components/side-nav";
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(true);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <header className="bg-cover bg-no-repeat bg-center bg-[url('/bg-image-4.webp')] h-[100vh]">
-      <div className="border-b p-4 flex flex-wrap md:flex-row items-center justify-between">
-        <span className="text-6xl text-white">Artesanal</span>
-        <button onClick={toggleMenu} className="mr-5 hover:text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="64"
-            height="64"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="#FFF"
-              d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41z"
-            />
-          </svg>
-        </button>
+    <header className="h-screen bg-cover bg-no-repeat bg-center bg-[url('/bg-image-4.webp')]">
+      <div className="h-24 border-b p-4 flex flex-wrap md:flex-row items-center justify-between">
+        <span className="text-4xl md:text-6xl text-white">Artesanal</span>
       </div>
-
-      {isOpen ? <MenuModal toggleMenu={toggleMenu} /> : ""}
+      <SideNav />
     </header>
   );
 }
--- app/components/third-section.tsx
+++ app/components/third-section.tsx
@@ -26,18 +26,18 @@ function third() {
       </section>
       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <div className="flex text-center lg:w-2/3 w-full">
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8">
             El trabajo manual artesanal permite la transmisión de conocimientos
             que hacen parte del patrimonio cultural inmaterial, fomenta la
             tradición, la sostenibilidad y la creación de comunidad.
           </p>
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8">
             América Latina es conocida por su rica diversidad cultural y
             tradiciones artesanales. Hay una amplia variedad de técnicas,
             materiales y estilos artesanales en toda la región, lo que brinda
             una gran cantidad de productos únicos y auténticos para ofrecer.
           </p>
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8">
             No obstante, nuestro sector se ve mediado por mucha precariedad,
             informalidad, autogestión que resta tiempo al creativo, carencia de
             canales, entre otras. Definir a un público resulta difícil,
